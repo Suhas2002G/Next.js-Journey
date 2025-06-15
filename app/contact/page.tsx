0a58997d8335e287a0dde0b0b7a606f339b849cb
suhas <suhas8838@gmail.com>
2025-06-15 22:45:23 +0530

    new changes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,12 +1,18 @@
-"use client";
+export default async function Contact() {
+    const res = await fetch('https://dummyjson.com/products');
+    const data = await res.json();
 
-export default function Contact() {
-    console.log('hey this is server or client component??')
+    console.log(data)
     return (
         <div>
             <h1>This is contact page</h1>
 
-
+            {data.products?.map((product) => (
+                <p key={product.id}>
+                    <strong>{product.title}</strong>
+                </p>
+            ))}
         </div>
     );
 }
+
